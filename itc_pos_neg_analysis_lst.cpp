9c56ba6288b644b7554a4209b4ae28e7fc5ae9de
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2022-01-20 20:05:46 +0300

    Update itc_pos_neg_analysis_lst.cpp

--- itc_pos_neg_analysis_lst.cpp
+++ itc_pos_neg_analysis_lst.cpp
@@ -2,7 +2,18 @@
 
 long long itc_max_positive_num(const vector<int> &lst){
     if(lst.size() == 0)
-        return 0;
+        return -1;
+    long long num = lst[0];
+    for(long long i = 0; i < lst.size(); i++){
+        if(lst[i] > num)
+            num = lst[i];
+    }
+    return num;
+}
+
+long long itc_max_negative_num(const vector<int> &lst){
+    if(lst.size() == 0)
+        return -1;
     long long num = lst[0];
     for(long long i = 0; i < lst.size(); i++){
         if(lst[i] > num)
@@ -13,7 +24,18 @@ long long itc_max_positive_num(const vector<int> &lst){
 
 long long itc_min_positive_num(const vector<int> &lst){
     if(lst.size() == 0)
-        return 0;
+        return -1;
+    long long num = lst[0];
+    for(long long i = 0; i < lst.size(); i++){
+        if(lst[i] < num)
+            num = lst[i];
+    }
+    return num;
+}
+
+long long itc_min_negative_num(const vector<int> &lst){
+    if(lst.size() == 0)
+        return -1;
     long long num = lst[0];
     for(long long i = 0; i < lst.size(); i++){
         if(lst[i] < num)
@@ -24,16 +46,19 @@ long long itc_min_positive_num(const vector<int> &lst){
 
 void itc_pos_neg_analysis_lst(const vector<int> &lst){
     setlocale(LC_ALL, "Russian");
-    long long posNumber = 0;
-    long long negNumber = 0;
     if(lst.size() == 0)
         return;
-    cout << "Ïîëîæèòåëüíûå:        Îòðèöàòåëüíûå:" << endl;
-    for(long long i = 0; i < lst.size(); i++){
-        if(lst[i] > 0)
-            posNumber++;
-        else if(lst[i] < 0)
-            negNumber++;
-    }
-    cout << "Êîëè÷åñòâî ÷èñåë: " << posNumber << ",        Êîëè÷åñòâî ÷èñåë: " << negNumber << ",";
+    vector<int> posNumbers;
+    vector<int> negNumbers;
+    vector<int> zeros;
+    itc_pos_neg_separator_lst(lst, negNumbers, zeros, posNumbers);
+    double posSize = posNumbers.size(), negSize = negNumbers.size(), posAverage = itc_sumlst(posNumbers) / posSize, negAverage = itc_sumlst(negNumbers) / negSize;
+    cout << "Положительные:			Отрицательные:" << endl;
+
+    cout << "Количество чисел: " << posNumbers.size() << ",        Количество чисел: " << negNumbers.size() << "," << endl;
+    cout << "Максимальная цифра: " << itc_max_positive_num(posNumbers) << ",		Максимальная цифра: " << itc_max_negative_num(negNumbers) << "," << endl;
+    cout << "Минимальная цифра: " << itc_min_positive_num(posNumbers) << ",		Минимальная цифра: " << itc_min_negative_num(negNumbers) << "," << endl;
+    cout << "Сумма чисел: " << itc_sumlst(posNumbers) << ",			Сумма чисел: " << itc_sumlst(negNumbers) << "," << endl;
+    cout << "Среднее значение: " << posAverage << " 		Среднее значение: " << negAverage << "," << endl;
+    cout << endl << "Количество нулей: " << zeros.size();
 }
