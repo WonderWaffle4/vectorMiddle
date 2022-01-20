93fe78b50e52e4fec82f684db26fdee02dbf6e7c
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2022-01-20 19:33:06 +0300

    Update itc_rmstrchar.cpp

--- itc_rmstrchar.cpp
+++ itc_rmstrchar.cpp
@@ -1,6 +1,19 @@
 #include "middle_list.h"
 
+bool isLetterInString(char character, string del){
+    for(long long i = 0; i < itc_len(del); i++){
+        if(character == del[i])
+            return true;
+    }
+
+    return false;
+}
+
 string itc_rmstrchar(string str, string del){
-    string s = "";
-    return s;
+    string newString = "";
+    for(long long i = 0; i < itc_len(str); i++){
+        if(!isLetterInString(str[i], del))
+            newString += str[i];
+    }
+    return newString;
 }
