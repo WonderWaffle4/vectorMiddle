abd44753cd28522da3d35935cd9553d598ddfacf
WonderWaffle4 <92299342+WonderWaffle4@users.noreply.github.com>
2022-01-20 12:45:05 +0300

    Update itc_pos_neg_separator_lst.cpp

--- itc_pos_neg_separator_lst.cpp
+++ itc_pos_neg_separator_lst.cpp
@@ -6,7 +6,7 @@ void itc_pos_neg_separator_lst(const vector<int> &lst, vector<int> &lst1, vector
             lst1.push_back(lst[i]);
         else if(lst[i] == 0)
             lst2.push_back(lst[i]);
-        else
+        else if(lst[i] > 0)
             lst3.push_back(lst[i]);
     }
 }
